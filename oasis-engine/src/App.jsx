e75f3dcd15c0234fbf1a901266fdbd5668ebfa86
xiexuanqian <1280809174@qq.com>
2022-08-19 17:21:22 +0800

    谢芊

---
+++ oasis-engine/src/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import Model from './models/model1'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Model/>
+      </div>
+    )
+  }
+}
